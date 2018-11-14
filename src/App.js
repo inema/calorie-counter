24133d5d60261d0cf04804899b13ff8cc27cefac
Isabelle <nema_i@hotmail.com>
2018-11-14 16:30:43 -0500

    title added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import Finder from './components/finder';
 import Tracker from './components/tracker';
-import logo from './gini.jpe';
 import './App.css';
 
 class App extends Component {
   render() {
     return (<div>
-      <img style={this.logoStyle} src={logo} alt="gini logo"/>
+      <h1>Calorie Counter</h1>
       <div className="flex-container">
         <Finder onTrack={this.handleTrack} />
         <Tracker ref="tracker" />
